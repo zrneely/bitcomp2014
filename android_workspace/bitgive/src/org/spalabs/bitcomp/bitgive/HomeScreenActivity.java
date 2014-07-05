0cac15cc43dad96b7403d1b94ddeab88c3872e94
Zachary Neely <zrneely@mit.edu>
2014-07-05 14:30:06 -0400

    Add the initial app and coding style.

---
+++ android_workspace/bitgive/src/org/spalabs/bitcomp/bitgive/HomeScreenActivity.java
@@ -0,0 +1,23 @@
+package org.spalabs.bitcomp.bitgive;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HomeScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_screen, menu);
+        return true;
+    }
+    
+}
