720d03122fda3fe6cf49973400da3c63bdef5165
Zachary Neely <zrneely@mit.edu>
2014-07-05 23:24:20 -0400

    Buttons!

--- android_workspace/bitgive/gen/org/spalabs/bitcomp/bitgive/R.java
+++ android_workspace/bitgive/gen/org/spalabs/bitcomp/bitgive/R.java
@@ -24,11 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int aboutButton=0x7f080003;
-        public static final int action_settings=0x7f080005;
+        public static final int aboutButton=0x7f080002;
+        public static final int action_settings=0x7f080004;
         public static final int giveButton=0x7f080000;
-        public static final int otherButton=0x7f080004;
-        public static final int settingsButton=0x7f080002;
+        public static final int settingsButton=0x7f080003;
         public static final int walletButton=0x7f080001;
     }
     public static final class layout {
